e82b0ad71659cde4f0614faa236cc8a9549da799
Tushar Nain <100490977+tusharnain4578@users.noreply.github.com>
2022-12-31 07:57:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { ReactDOM, useState } from "react";
+import $ from "jquery";
+
+function App() {
+
+  const [name, setName] = useState("");
+
+  const [user, setUser] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    $.ajax({
+      url: "https://zany-pocket-jay.cyclic.app/",
+      xhrFields: {
+        withCredentials: true
+      },
+      method: "POST",
+      data: {
+        name: name
+      },
+      success: function (data) {
+        if (data) {
+          console.log(data);
+          setUser(data)
+        } else {
+          console.log("something wrong with received data");
+        }
+      }
+    })
+  }
+
+  if (user == "") {
+    return (
+      <>
+        <form method="POST" onSubmit={handleSubmit}>
+          <input type="text" name="name" onChange={handleName} />
+          <button name="submit" value="submit"> Submit </button>
+        </form>
+
+      </>
+    );
+  }
+
+
+  return (
+    <>
+      <h1>{user}</h1>
+
+    </>
+  );
+
+
+}
+
+export default App;
